4e55afaad6bf17bf70af4eb6b68e78a5b2eed3cc
refschool <referencementschool@gmail.com>
2017-02-21 20:47:39 +0100

    fix redirect

--- manage/write.php
+++ manage/write.php
@@ -10,7 +10,7 @@ if(!isset($_GET['id'])){$editid='1';} else {$editid = $_GET['id'];}
 
 if(!isset($_SESSION['LoggedIn'])){
 	echo 'u are not logged in please log in';
-echo "<meta http-equiv='refresh' content='2;'/manage/index.php' />";
+echo "<meta http-equiv='refresh' content='2;".$base_url."/manage/index.php' />";
 
 }
 	
@@ -18,7 +18,7 @@ if(isset($_SESSION['Userlevel']) &&  $_SESSION['Userlevel'] <> 'admin'){
 echo 'user level = '. $_SESSION['Userlevel'];
 	echo "Vous n'avez pas les droits suffisants pour éditer !<br> ";
 	echo '<a href="'.$tld.'">Retour</a><br>';
-	echo '<a href="'.$tld.'manage/logout.php">Logout</a>';
+	echo '<a href="<?=$base_url?>/manage/logout.php">Logout</a>';
 }
 
 else {
@@ -37,7 +37,7 @@ else {
 <script type="text/javascript" src="js/js/jquery-ui-1.8.15.custom.min.js"></script>
 <script src="<?=$base_url."/manage/lib/tinymce/js/tinymce/tinymce.min.js"?>"></script>
 <link rel="stylesheet" href="<?=$base_url."/inc/uikit/css/uikit.min.css" ?>"/>
-
+<script type="text/javascript"  src="<?=$base_url."/inc/uikit/js/uikit.min.js" ?>"></script>
 <script type="text/javascript" src="<?=$base_url."/manage/inc/js/write.js"?>"></script>
 <script type="text/javascript">
 	$(function() {
@@ -59,9 +59,7 @@ else {
 	{
 ?>
 <body onload="updatelist('<?php if(empty($_SESSION['category'])){echo 'all'; } else{echo $_SESSION['category'];} ?>');">
-<div class="uk-width-1-1">
-	
-	<!-- END OF HEADER DIV  -->
+
 	<div id="header" class="uk-width-1-1">
 		<div id="menu">
 			<?php
@@ -69,34 +67,53 @@ else {
 			?>
 		</div>
 		<br style="clear:both" />
-	</div>
+	</div><!-- END OF HEADER DIV  -->
 
-	<!-- EDITOR DIV  -->
 
-	<div class="uk-grid" id="editor" style="z-index:9;display:block">
-		<div class="uk-width-1-1">
-				<?php
-				include("includes/editor.php");
-				?>
+		<!-- test DIV  -->
+	<div class="uk-grid" >
+		<div class="uk-width-3-4">
+			<div class="uk-card uk-card-default uk-card-body">
+
+			<div id="editor" style="z-index:9;display:block" class="uk-grid">
+				<div class="uk-width-1-1">
+					<?php
+					include("includes/editor.php");
+					?>
+				</div>
+			</div>
+
+			<div class="uk-grid" id="browser" style="display:none;z-index:10">
+				<div class="uk-width-1-1">
+					<?php
+					include("includes/browser.php");
+					?>
+				</div>
+			</div>			
+
+			</div>
+
+		</div>
+
+
+		<div class="uk-width-1-4">
+			<div class="uk-card uk-card-default uk-card-body">sidebar</div>
+				
+
+			<div class="uk-card uk-card-default uk-card-body">sidebar</div>
 		</div>
 	</div>
 
-	<!-- BROWSER DIV  -->
 
-	<div class="uk-grid" id="browser" style="display:none;z-index:10">
-		<div class="uk-width-1-1">
-			<?php
-			include("includes/browser.php");
-			?>
+
+		<div>
+
+			<div id="footer">
+				<?=get_manager_version();?>
+			</div>
 		</div>
+
 	</div>
-
-	<div id="footer">
-	<?=get_manager_version();?>
-	</div>
-
-</div>
-
 
 </body>
 <?php
