7fe821ebb006f1257b5222929f0ead2a0f87f326
refschool <referencementschool@gmail.com>
2017-05-14 11:00:33 +0200

    add image insertion and code edit

--- manage/inc/js/write.js
+++ manage/inc/js/write.js
@@ -1,6 +1,6 @@
 tinymce.init({
   selector: 'textarea',
-  plugins: 'codesample,link,preview,wordcount,save',
+  plugins: 'codesample,link,preview,wordcount,save,code,image',
   codesample_languages: [
         {text: 'HTML/XML', value: 'markup'},
         {text: 'JavaScript', value: 'javascript'},
@@ -13,5 +13,7 @@ tinymce.init({
         {text: 'C#', value: 'csharp'},
         {text: 'C++', value: 'cpp'}
     ],
-  toolbar: 'codesample,link,preview,wordcount,undo,redo,formatselect,save'
+  toolbar: 'codesample,link,preview,wordcount,undo,redo,formatselect,save,code,image',
+  image_caption: true,
+  image_prepend_url: "http://www.tinymce.com/images/"
 });
